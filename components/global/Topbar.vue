fd2beefd5e085167e97059a838d875e144e68c08
Shubham Kaushal <63925481+shubham-kaushal@users.noreply.github.com>
2021-02-10 23:22:36 +0530

    Update Topbar.vue

--- components/global/Topbar.vue
+++ components/global/Topbar.vue
@@ -9,28 +9,5 @@
       <nu-pane text="nowrap">
         <nu-svg src="/images/forneu-logo-small.svg" height="40px" width="132.38px" />
       </nu-pane>
-      <nu-pane size="lg" gap="0|||1x">
-        <nu-attrs for="btn" padding="1x 2x|||1x" />
-        <nu-attrs for="icon" show="y||||n" />
-        <nu-btn clear to="!https://www.instagram.com/forneuhq">
-          <nu-icon name="logo-instagram" />
-          Instagram
-        </nu-btn>
-        <nu-btn clear to="https://www.facebook.com/forneuhq">
-          <nu-icon name="logo-facebook" />
-          Facebook
-        </nu-btn>
-        <nu-btn clear to="!https://twitter.com/forneuhq">
-          <nu-icon name="logo-twitter" />
-          Twitter
-        </nu-btn>
-      </nu-pane>
-      <!--      <nu-block>-->
-      <!--        <nu-inputgroup radius="round">-->
-      <!--          <nu-icon name="search-outline"></nu-icon>-->
-      <!--          <nu-search />-->
-      <!--        </nu-inputgroup>-->
-      <!--      </nu-block>-->
-    </nu-pane>
   </nu-header>
 </template>
