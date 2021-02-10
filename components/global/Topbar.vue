a2d05b8fb4b2c29877025e936dd3ed271a9410da
Shubham Kaushal <63925481+shubham-kaushal@users.noreply.github.com>
2021-02-10 23:36:49 +0530

    Update Topbar.vue

--- components/global/Topbar.vue
+++ components/global/Topbar.vue
@@ -5,8 +5,7 @@
     padding="4x 1cp||2x 1cp"
     fill="clear"
   >
-    <nu-pane content="space-between" size="md" flow="row|||column" gap="2x 1x">
-      <nu-pane text="nowrap">
+    <nu-pane content="space-between">
         <nu-svg src="/images/forneu-logo-small.svg" height="40px" width="132.38px" />
       </nu-pane>
   </nu-header>
