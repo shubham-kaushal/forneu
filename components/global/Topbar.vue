8637c9836e2ee84be3139be1f8646cd802ea5144
Shubham Kaushal <63925481+shubham-kaushal@users.noreply.github.com>
2021-02-11 11:53:44 +0530

    Update Topbar.vue

--- components/global/Topbar.vue
+++ components/global/Topbar.vue
@@ -5,7 +5,7 @@
     padding="4x 1cp||2x 1cp"
     fill="clear"
   >
-    <nu-pane items="center">
+    <nu-pane content="center">
         <nu-svg src="/images/forneu-logo-small.svg" height="40px" width="132.38px" />
       </nu-pane>
   </nu-header>
