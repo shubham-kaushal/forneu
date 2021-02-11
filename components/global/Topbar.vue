1d2c5afa2ea02d056a11cff9cea9279bc02a419c
Shubham Kaushal <63925481+shubham-kaushal@users.noreply.github.com>
2021-02-11 19:55:26 +0530

    Update Topbar.vue

--- components/global/Topbar.vue
+++ components/global/Topbar.vue
@@ -8,5 +8,16 @@
     <nu-pane content="center">
         <nu-svg src="/images/forneu-logo-small.svg" height="40px" width="132.38px" />
       </nu-pane>
+    <SiteBlock
+        badge="serverless framework · cms · eCommerce"
+        heading="Build your web or mobile apps in minutes"
+        description="Highly-scalable & highly-available out of the box."
+        padding="10x 0||8x 0|4x 0 8x"
+        level="1"
+        fill="clear"
+        special
+        border="n"
+      >
+      </SiteBlock>
   </nu-header>
 </template>
